bcd6db1198dcece7908f5c8d95c0d3917398530c
unknown <nhtri2003@gmail.com>
2020-08-23 16:57:23 +0700

    change user

--- Run.py
+++ Run.py
@@ -1,9 +1,11 @@
 import os
+import getpass
 
 addedFiles = []
 oldPath = 'Data\\Old\\'
 newPath = 'Data\\New\\'
 files = ['ProgramFiles.txt', 'ProgramFilesx86.txt', 'ProgramData.txt', 'AppData.txt']
+username = getpass.getuser()
 
 def Create_Folders():
     if (not os.path.exists('Data')):
@@ -18,13 +20,13 @@ def Get_Old_Files():
     os.system(f'dir /s /b "C:\\Program Files" > {oldPath}{files[0]}')
     os.system(f'dir /s /b "C:\\Program Files (x86)" > {oldPath}{files[1]}')
     os.system(f'dir /s /b "C:\\ProgramData" > {oldPath}{files[2]}')
-    os.system(f'dir /s /b "C:\\Users\\JohnathanHuuTri\\AppData" > {oldPath}{files[3]}')
+    os.system(f'dir /s /b "C:\\Users\\{username}\\AppData" > {oldPath}{files[3]}')
 
 def Get_New_Files():
     os.system(f'dir /s /b "C:\\Program Files" > {newPath}{files[0]}')
     os.system(f'dir /s /b "C:\\Program Files (x86)" > {newPath}{files[1]}')
     os.system(f'dir /s /b "C:\\ProgramData" > {newPath}{files[2]}')
-    os.system(f'dir /s /b "C:\\Users\\JohnathanHuuTri\\AppData" > {newPath}{files[3]}')
+    os.system(f'dir /s /b "C:\\Users\\{username}\\AppData" > {newPath}{files[3]}')
 
 def Check_Files():
     for file in files:
